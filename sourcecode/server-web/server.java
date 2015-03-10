3be3e5ae9e56aea3c6e77e4da0fcc15c74e61e2c
MrChimick <mikechimick@gmail.com>
2015-03-09 18:30:49 -0700

    server theoretically works

--- sourcecode/server-web/server.java
+++ sourcecode/server-web/server.java
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------------
---  Source File:        udps.java - a simple Java UDP (multi-threaded) echo server
+--  Source File:        Server.java - a simple Java UDP (multi-threaded) echo server
 --
---  Classes:        udps        - public class
+--  Classes:        Server        - public class
 --              DatagramSocket  - java.net
 --              DatagramPacket  - java.net
 --              
@@ -32,89 +32,169 @@
 --  displaying it. 
 --
 --  Generate the class file and run it as follows:
---          javac udps
---          java udps <server port>
+--          javac Server
+--          java Server <server port>
 ---------------------------------------------------------------------------------------*/
 
-import java.net.*;
-import java.io.*;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.String;
 
-public class udps extends Thread
+public class Server extends Thread
 {
-    String ClientString;
-    int ClientPort;
+    private String ClientString;
+    private int ClientPort;
     private DatagramSocket ListeningSocket;
-    DatagramPacket dgram;
-    static final int DgramSize = 1024;
-    byte[] PacketData;
-    InetAddress Addr;
+    private DatagramPacket dgram;
+    private static final int DgramSize = 1024;
+    private byte[] PacketData;
+    private InetAddress Addr;
 
-    public udps (int port) throws IOException
+    public Server (int port) throws IOException
     {
         ListeningSocket = new DatagramSocket (port);
-        //ListeningSocket.setSoTimeout(30000); // set a 20 second timeout
     }
 
     public void run()
     {
         while(true)
         {
-          PacketData = new byte[DgramSize];
-          dgram = new DatagramPacket (PacketData, DgramSize);
-          try
-          {
-              // Listen for datagrams
-              //System.out.println ("Listening on port: " + ListeningSocket.getLocalPort());
-              ListeningSocket.receive(dgram);
-              //Addr = dgram.getAddress();
-              //ClientPort = dgram.getPort();
-              //System.out.println ("Datagram from: "+ Addr +":"+ ClientPort);
-              
-              // Get the client data
-              ClientString = new String (PacketData, 0, dgram.getLength());
-              System.out.println ("Message: "+ ClientString.trim());
-              
-              // Echo it back
-              //dgram = new DatagramPacket(PacketData, DgramSize, Addr, ClientPort);
-              
-              /*try
-              {
-                  ListeningSocket.send (dgram);
-              }
-              catch(IOException ex)
-              {
-                  System.out.println(" Could not Send :"+ex.getMessage());
-                  System.exit(0);
-              }*/
-          }
+            PacketData = new byte[DgramSize];
+            dgram = new DatagramPacket (PacketData, DgramSize);
+            try
+            {
+                // Listen for datagrams
+                System.out.println("Port: " + ListeningSocket.getLocalPort());
+                ListeningSocket.receive(dgram);
+                Addr = dgram.getAddress();
+                ClientPort = dgram.getPort();
+                System.out.println ("Datagram from: "+ Addr +":"+ ClientPort);
+                
+                // Get the client data
+                ClientString = new String (PacketData, 0, dgram.getLength());
+                System.out.println ("Message: "+ ClientString.trim());
+
+            }
     
-              catch (SocketTimeoutException s)
-              {
-                  System.out.println ("Socket timed out!");
-                  ListeningSocket.close();
-                  break;
-              }
-    
-              catch(IOException e)
-              {
-                  e.printStackTrace();
-                  break;
-              }
+            catch (SocketTimeoutException s)
+            {
+                System.out.println ("Socket timed out!");
+                ListeningSocket.close();
+                break;
+            }
+  
+            catch(IOException e)
+            {
+                e.printStackTrace();
+                break;
+            }
         }
+    }
+
+    public void addPoint(String message)
+    {
+        FileInputStream in;
+        FileOutputStream out;
+        int skipLength = 0;
+        String point;
+
+        try
+        {
+            in = new FileInputStream("data.xml");
+            skipLength = in.available() - 8;
+            in.close();
+
+            out = new FileOutputStream("data.xml");
+            point = getPoint(message);
+            out.write(point.getBytes(), skipLength, point.length());
+        }
+        catch(Exception e)
+        {
+            die("addPoint failure");
+        }
+    }
+
+    public String getPoint(String message) 
+    {
+        String err = "File missing fields";
+
+        String name;
+        String ip;
+        String time;
+        String lat;
+        String lon;
+
+        int index = 0;
+        int nextIndex = 0;
+
+        nextIndex = getNextDivider(message.toCharArray(), index);
+        if (index < 0) die(err);
+        name = message.substring(index, nextIndex - 1);
+        index = nextIndex + 1;
+
+        nextIndex = getNextDivider(message.toCharArray(), index);
+        if (index < 0) die(err);
+        ip = message.substring(index, nextIndex - 1);
+        index = nextIndex + 1;
+
+        nextIndex = getNextDivider(message.toCharArray(), index);
+        if (index < 0) die(err);
+        time = message.substring(index, nextIndex - 1);
+        index = nextIndex + 1;
+
+        nextIndex = getNextDivider(message.toCharArray(), index);
+        if (index < 0) die(err);
+        lat = message.substring(index, nextIndex - 1);
+        index = nextIndex + 1;
+
+        nextIndex = getNextDivider(message.toCharArray(), index);
+        if (index < 0) die(err);
+        lon = message.substring(index, nextIndex - 1);
+        index = nextIndex + 1;
+
+        return "    <user>" +
+               "        <name>"      + name + "</name>" +
+               "        <ip>"        + ip   + "</ip>" +
+               "        <time>"      + time + "</time>" +
+               "        <latitude>"  + lat  + "</latitude>" +
+               "        <longitude>" + lon  + "</longitude>" +
+               "    </user>" +
+               "</users>";
+    }
+
+    public int getNextDivider(char[] message, int start)
+    {
+        for (int i = start; i < message.length; i++)
+        {
+            if (message[i] == '|') return i;
+        }
+
+        return -1;
+    }
+
+    public void die(String message)
+    {
+        System.out.println(message);
+        System.err.println(message);
+        System.exit(-1);
     }
 
     public static void main (String [] args)
     {
+        int port = 0;
         if(args.length != 1)
-            {
-                System.out.println("Usage Error : java udps <port>");
-                System.exit(0);
-            }   
-            int port = Integer.parseInt(args[0]);
+            port = 7000;
+        else
+            port = Integer.parseInt(args[0]);
        
         try
         {
-            Thread t = new udps (port);
+            Thread t = new Server (port);
             t.start();
         }
         
